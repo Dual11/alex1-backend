01697e2bcd8e81142f214aa66928df82f5f89519
alex01 <alextobar013@gmail.com>
2026-02-11 07:39:49 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+
     // Aquí irán las rutas protegidas de relojes
     Route::middleware('auth:api')->apiResource('watches', Api\WatchController::class);
 });
